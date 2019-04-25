f71279dbebeae12acae8b992ec7aa2b536e7cec5
aspire_zubair <zubair@aspirelogics.com>
2019-04-25 12:54:39 +0500

    router has been added, with multiple component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,8 +6,10 @@
  */
 
 require('./bootstrap');
+import VueRouter from 'vue-router'
 
 window.Vue = require('vue');
+Vue.use(VueRouter);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +17,29 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+const example = require('./components/ExampleComponent.vue');
+const about = require('./components/AboutComponent.vue');
+
+const routes = [
+
+    {
+        path: '/example',
+        component: example
+    },
+    {
+        path : '/about',
+        component : about
+    }
+
+]
+
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+const router = new VueRouter({
+    routes
+})
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,20 +11,41 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
-
         
     </head>
+    
     <body>
-        
-        <div class="container">
-            <h4>Laravel Vue</h4>
-        </div>
-
         <div id="app">
-           <example-component></example-component>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Brand</a>
+              </div>
+          
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                
+                
+                <ul class="nav navbar-nav navbar-right">
+                  <li><router-link to="/example" >Example</router-link></li>
+                  <li><router-link to="/about" >About </router-link></li>
+                  
+                </ul>
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+          </nav>
+
+
+          <router-view></router-view>
+
         </div>
-
-
     <script src="{{asset('js/app.js')}}"></script>
     </body>
 </html>
